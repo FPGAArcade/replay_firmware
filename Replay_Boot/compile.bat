3cc7f6393e086f32f855914220d07e9ddffd38cd
Wolfgang Scherr <ws_arcade@pin4.at>
2013-12-16 21:39:20 +0000

    Fixed build process. Firmware compile generates s19 file for Replay_Update tool and for the rAppFlashUpdater the same time, also launches the ini generator to set up the sdcard files for this rApp.

--- Replay_Boot/compile.bat
+++ Replay_Boot/compile.bat
@@ -2,4 +2,8 @@ if exist c:\freeware\set_path_arm.bat call c:\freeware\set_path_arm
 make
 copy /Y /B ..\..\bootloader\replay_loader_v1.2\usbdl.exe ..\Replay_Update\FW
 copy /Y /B build\main.s19 ..\Replay_Update\FW
+copy /Y /B build\main.bin ..\Replay_Apps\rAppFlashUpdater\sdcard
+cd ..\Replay_Apps\rAppFlashUpdater\sdcard
+copy /Y ..\rApp_template.ini rApp.ini
+..\..\..\..\tools\genupd\genupd.exe >> rApp.ini
 pause

d904410e141aabc9c3e17332b4e27352271b5351
Johan Svensson <johan.svensson@git-migration.fpgareplay.com>
2014-03-15 13:56:17 +0000

    Adding GNU/Linux support for making buildnum (make linux)

--- tools/buildnumber/buildnum.c
+++ tools/buildnumber/buildnum.c
@@ -1,4 +1,8 @@
+#ifdef __linux
+#include <time.h>
+#else
 #include<Windows.h>
+#endif
 #include<stdio.h>
 // Build-Number-Generator
 //
@@ -13,7 +17,17 @@
 main() {
     FILE *buildFile;
     int buildNum;
+    char buildDate[10];
+
+#ifdef __linux
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    sprintf(buildDate, "%04d%02d%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday);
+#else
     SYSTEMTIME sysTime;
+    GetLocalTime(&sysTime);
+    sprintf(buildDate, "%04d%02d%02d",sysTime.wYear,sysTime.wMonth,sysTime.wDay);
+#endif
 
     buildFile=fopen("buildfile.num","rb");
     if (!buildFile) {
@@ -24,8 +38,7 @@ main() {
         buildNum = (buildNum+1)%1000;
     }
 
-    GetLocalTime(&sysTime);
-    printf("%04d%02d%02d_%03d",sysTime.wYear,sysTime.wMonth,sysTime.wDay,buildNum);
+    printf("%s_%03d", buildDate, buildNum);
 
     buildFile=fopen("buildfile.num","wb");
     if (buildFile) {

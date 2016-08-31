37c1e4115617425fff3cceef7149e97c602e701c
Erik Hemming <eriquesnk@gmail.com>
2016-08-31 06:15:47 +0000

    Enforce consecutive buildnums; allow parallel make; enable building from osx/linux as well

--- tools/buildnumber/buildnum.c
+++ tools/buildnumber/buildnum.c
@@ -1,9 +1,9 @@
-#ifdef __linux
+#if !defined(WIN32)
 #include <time.h>
 #else
-#include<Windows.h>
+#include <Windows.h>
 #endif
-#include<stdio.h>
+#include <stdio.h>
 // Build-Number-Generator
 //
 // (c) W. Scherr ws_arcade <at> pin4.at
@@ -14,12 +14,12 @@
 // $id:$
 //
 
-main() {
+int main() {
     FILE *buildFile;
     int buildNum;
     char buildDate[10];
 
-#ifdef __linux
+#if !defined(WIN32)
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
     sprintf(buildDate, "%04d%02d%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday);
@@ -38,7 +38,7 @@ main() {
         buildNum = (buildNum+1)%1000;
     }
 
-    printf("%s_%03d", buildDate, buildNum);
+    printf("%s_%03d%c", buildDate, buildNum, 0);
 
     buildFile=fopen("buildfile.num","wb");
     if (buildFile) {

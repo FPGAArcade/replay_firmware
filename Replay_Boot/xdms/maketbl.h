2fc9410ec2b1e840b208ce4f2eba7690ced17846
Erik Hemming <eriquesnk@gmail.com>
2017-08-14 09:01:15 +0000

    Removed unused xdms
    Former-commit-id: 86d07cdd046624ab1bc942e7b9237a2c29eddf09

--- Replay_Boot/xdms/maketbl.h
+++
@@ -1,4 +0,0 @@
-
-
-USHORT make_table(USHORT nchar, UCHAR bitlen[], USHORT tablebits, USHORT table[], USHORT left[], USHORT right[]);
-
--- Replay_Boot/xdms/u_heavy.h
+++
@@ -1,7 +0,0 @@
-
-
-USHORT Unpack_HEAVY(UCHAR *, UCHAR *, UCHAR, USHORT);
-
-extern int init_heavy_tabs;
-extern USHORT heavy_text_loc;
-

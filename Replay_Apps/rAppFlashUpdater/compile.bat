71c63de71d8d536d96746903db525dc43d6c6367
Erik Hemming <eriquesnk@gmail.com>
2017-10-12 20:36:59 +0000

    Copy the latest loader core in compile.bat
    Former-commit-id: 348b02ab6f2e7b5e8911fc57e55cb9ee73eb1ba7

--- Replay_Apps/rAppFlashUpdater/compile.bat
+++ Replay_Apps/rAppFlashUpdater/compile.bat
@@ -2,4 +2,5 @@ rem $Id$
 if exist c:\freeware\set_path_arm.bat call c:\freeware\set_path_arm
 make
 copy build\main.bin sdcard\rApp_ARM.bin
+copy ..\..\..\..\hw\replay\cores\loader\sdcard\loader.bin sdcard\rApp_FPGA.bin
 pause

9e256cfa093f017698c9552a5c28a29fecbe7a70
Erik Hemming <eriquesnk@gmail.com>
2019-04-24 22:27:33 +0200

    Print some early debug output on serial1

--- Replay_Boot/Replay_Boot.ino
+++ Replay_Boot/Replay_Boot.ino
@@ -2,6 +2,16 @@
 #include "main.h"
 
 void setup() {
+
+  char buf[256];
+  sprintf(buf, "FPGAArcade Replay VIDOR %s %s", __DATE__, __TIME__);
+
+  Serial1.begin(115200);
+  Serial1.println("\033[2J");
+  Serial1.println("=========================================================");
+  Serial1.println(buf);
+  Serial1.println("=========================================================");
+  Serial1.flush();
 }
 
 void loop() {

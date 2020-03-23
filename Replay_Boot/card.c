661b973e74c167c4ea2d5b7483b5daba075f5d2d
Erik Hemming <eriquesnk@gmail.com>
2020-03-23 20:04:17 +0100

    Fix HOSTED build
    astyle

--- Replay_Boot/card.c
+++ Replay_Boot/card.c
@@ -439,6 +439,8 @@ FF_T_SINT32 Card_ReadM(FF_T_UINT8* pBuffer, FF_T_UINT32 sector, FF_T_UINT32 numS
 #else
         (void) dma_end;
 
+        (void) dma_buffer;
+
         // read sector bytes
         if (pBuffer) {
             for (uint32_t offset = 0; offset < 512; offset++) {

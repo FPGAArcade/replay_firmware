1828bc569697602f9120a2ccd79aae8f3ae0485e
Erik Hemming <eriquesnk@gmail.com>
2020-12-12 12:37:03 +0100

    Add MPL2 license header

--- Replay_Boot/replay.h
+++ Replay_Boot/replay.h
@@ -1,3 +1,19 @@
+/*--------------------------------------------------------------------
+ *                       Replay Firmware
+ *                      www.fpgaarcade.com
+ *                     All rights reserved.
+ *
+ *                     admin@fpgaarcade.com
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *--------------------------------------------------------------------
+ *
+ * Copyright (c) 2020, The FPGAArcade community (see AUTHORS.txt)
+ *
+ */
+
 #pragma once
 
 #ifdef __cplusplus

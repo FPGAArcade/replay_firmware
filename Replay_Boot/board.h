6f6af1c32931c2c609b07a46f579dfba9b575396
Erik Hemming <eriquesnk@gmail.com>
2021-04-23 17:05:17 +0200

    Fix ACTLED_OFF / ACTLED_ON

--- Replay_Boot/board.h
+++ Replay_Boot/board.h
@@ -245,8 +245,8 @@
 #endif // defined(AT91SAM7S256)
 
 // Macros to toggle state of activity LED on board
-#define ACTLED_OFF do { IO_DriveLow_OD(PIN_ACT_LED); } while (0)
-#define ACTLED_ON  do { IO_DriveHigh_OD(PIN_ACT_LED); } while (0)
+#define ACTLED_OFF do { IO_SetOutputData(PIN_ACT_LED); } while (0)
+#define ACTLED_ON do { IO_ClearOutputData(PIN_ACT_LED); } while (0)
 
 //---------------------------------------------------------------------------
 

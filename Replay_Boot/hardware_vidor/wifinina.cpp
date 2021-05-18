0b713ee14cd3e6e10bcb69e4aae75aa1658d4517
Erik Hemming <eriquesnk@gmail.com>
2021-05-18 23:15:22 +0200

    Add support for newer NINA firmware

--- Replay_Boot/hardware_vidor/wifinina.cpp
+++ Replay_Boot/hardware_vidor/wifinina.cpp
@@ -14,6 +14,31 @@
  *
  */
 
+/*
+  The latest offical NINA firmware (1.1.0) does not support BT (nor does it 
+  have the recent bug fixes).
+
+  Instead download it from: https://github.com/arduino/nina-fw/releases
+
+  And flash it using the EspressIf ESP32 IDF tools:
+
+  $ python3 $IDF_PATH/components/esptool_py/esptool/esptool.py              \
+            --no-stub                                                       \
+            --before default_reset --after hard_reset write_flash           \
+            -z --flash_mode dio --flash_freq 40m --flash_size detect 0x0    \
+            ~/Downloads/NINA_W102-v1.4.5-UNO-WIFI-REV2.bin
+
+  (You need to be running the SerialNINAPassthrough.ino example at this point)
+
+  Note that we're using the UNI-WIFI-REV2 firmware. There is a reason for that...
+  This firmware configures the BT interface on the DEBUG/FLASH UART, while
+  keeping the SPI interface (for WIFI) the same as the SDCARD/FPGA.
+
+  ( see https://github.com/arduino/nina-fw/blob/master/main/sketch.ino.cpp#L121-L122 )
+
+  This way we can use SERCOM3 to talk to it, if we run it in BT mode.
+*/
+
 #include "SPI.h"
 #include <wiring_private.h>
 
@@ -34,9 +59,28 @@ extern "C" {
 
 extern "C" void SPI_DMA(const void* out, void* in, uint16_t length);
 
+static void ResetNina(bool wifi)
+{
+    // PIN_NINA_CS_L == PIN_FPGA_CTRL0 + PIN_FPGA_CTRL1
+    // NINA CS high during reset == WIFI mode
+    // NINA CS low during reset == BT mode
+    const uint8_t cs = wifi ? HIGH : LOW;
+    digitalWrite(PIN_FPGA_CTRL1, cs);
+    digitalWrite(PIN_FPGA_CTRL0, cs);
+
+    digitalWrite(PIN_NINA_RST_L, LOW);
+    delay(10);
+    digitalWrite(PIN_NINA_RST_L, HIGH);
+    // takes about 500ms for NINA to reach the point where it samples BT/WIFI
+    delay(500);
+
+    digitalWrite(PIN_FPGA_CTRL1, HIGH);
+    digitalWrite(PIN_FPGA_CTRL0, HIGH);
+}
+
 static void EnableNina()
 {
-    SPI.beginTransaction(SPISettings(8000000, MSBFIRST, SPI_MODE0));
+    SPI.beginTransaction(SPISettings(6000000, MSBFIRST, SPI_MODE0));
     digitalWrite(PIN_FPGA_CTRL0, LOW);
     digitalWrite(PIN_FPGA_CTRL1, LOW);
 }
@@ -78,9 +122,7 @@ static bool Wait(uint8_t c)
     return true;
 }
 
-static uint8_t nina_response[32];
-
-static const uint8_t* SPI_NINA(uint8_t* cmd, size_t length)
+static const uint8_t* SPI_NINA(uint8_t* cmd, size_t length, uint8_t* buffer, size_t buffer_size)
 {
     if (length < 3) {
         return 0;
@@ -124,8 +166,8 @@ static const uint8_t* SPI_NINA(uint8_t* cmd, size_t length)
 
         if (rSPI(0xff) == (op | 0x80)) {
             uint8_t num_params = rSPI(0xff);
-            uint8_t* p = &nina_response[0];
-            size_t left = sizeof(nina_response);
+            uint8_t* p = &buffer[0];
+            size_t left = buffer_size;
 
             while (num_params--) {
                 uint8_t len = rSPI(0xff);
@@ -155,7 +197,7 @@ static const uint8_t* SPI_NINA(uint8_t* cmd, size_t length)
 
         DisableNina();
     }
-    return nina_response;
+    return buffer;
 }
 
 namespace nina
@@ -169,7 +211,8 @@ static void setDebug(uint8_t debug)
         sizeof(debug), debug,
         NINA_END
     };
-    SPI_NINA(cmd, sizeof(cmd));
+    uint8_t response[32];
+    SPI_NINA(cmd, sizeof(cmd), response, sizeof(response));
 }
 
 static const char* getVersion()
@@ -180,7 +223,8 @@ static const char* getVersion()
         0/*params*/,
         NINA_END
     };
-    return (const char*)SPI_NINA(cmd, sizeof(cmd));
+    uint8_t response[32];
+    return (const char*)SPI_NINA(cmd, sizeof(cmd), response, sizeof(response));
 }
 
 static void pinMode(uint8_t pin, uint8_t mode)
@@ -193,7 +237,8 @@ static void pinMode(uint8_t pin, uint8_t mode)
         sizeof(mode), mode,
         NINA_END
     };
-    SPI_NINA(cmd, sizeof(cmd));
+    uint8_t response[32];
+    SPI_NINA(cmd, sizeof(cmd), response, sizeof(response));
 }
 
 static void digitalWrite(uint8_t pin, uint8_t val)
@@ -206,7 +251,8 @@ static void digitalWrite(uint8_t pin, uint8_t val)
         sizeof(val), val,
         NINA_END
     };
-    SPI_NINA(cmd, sizeof(cmd));
+    uint8_t response[32];
+    SPI_NINA(cmd, sizeof(cmd), response, sizeof(response));
 }
 
 }
@@ -220,26 +266,32 @@ void SERCOM3_Handler()
 
 extern "C" void NINA_Update()
 {
-    // Check if NINA is available and ready
-    if (!IO_Input_L(PIN_NINA_RDY_L)) {
-        return;
-    }
-
     static uint8_t _init = 0;
 
-    if (!_init) {
+    if (_init == 0) {
+        ResetNina(true);
+
         pinPeripheral(PIN_NINA_RX, PIO_SERCOM);
         pinPeripheral(PIN_NINA_TX, PIO_SERCOM);
         SerialNina.begin(115200);
         SerialUSB.begin(115200);
 
-        INFO("NINA: %s\n", nina::getVersion());
+        _init++;
+    }
+
+    // Check if NINA is available and ready
+    if (!IO_Input_L(PIN_NINA_RDY_L)) {
+        return;
+    }
+
+    if (_init == 1) {
         nina::setDebug(debuglevel >= 1);
+        INFO("NINA: %s\n", nina::getVersion());
 
         nina::pinMode(PIN_LED_G_NINA, OUTPUT);
         nina::pinMode(PIN_LED_B_NINA, OUTPUT);
 
-        _init = 1;
+        _init++;
     }
 
     {

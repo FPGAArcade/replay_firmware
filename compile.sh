81532b3eee0cc2c3efb749e6ce4da95a68293e54
Erik Hemming <eriquesnk@gmail.com>
2022-02-26 00:36:02 +0100

    Output build tag from compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+GIT=`git describe --tag --always --dirty="+"`
+
 set -ex
 cd Replay_Boot && ./compile.sh && cd ..
 cd Replay_Apps && ./compile.sh && cd ..
+
+set +ex
+echo
+echo Build DONE : "${GIT}"
+echo
